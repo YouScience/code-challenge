d9247150b20314e922062933abfcb3930dd80471
Divyarani <137287563+Divyarani3020@users.noreply.github.com>
2023-06-23 23:10:10 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-    const webpack = require('webpack');
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const dotenv = require('dotenv');
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dotenv = require("dotenv");
 
 module.exports = () => {
   const env = dotenv.config().parsed;
@@ -12,22 +12,24 @@ module.exports = () => {
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
   }, {});
-  const isDev = process.env.NODE_ENV === 'development';
+  const isDev = process.env.NODE_ENV === "development";
 
   return {
-    mode: isDev ? 'development' : 'production',
+    mode: isDev ? "development" : "production",
     // optimization: {},
-    optimization: isDev ? {} : { minimize: true, minimizer: [new CssMinimizerPlugin()] },
+    optimization: isDev
+      ? {}
+      : { minimize: true, minimizer: [new CssMinimizerPlugin()] },
     entry: {
-      index: './src/index.jsx',
+      index: "./src/index.tsx",
     },
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: isDev ? 'js/[name].js' : 'js/[name].[fullhash:4].js',
+      path: path.resolve(__dirname, "build"),
+      filename: isDev ? "js/[name].js" : "js/[name].[fullhash:4].js",
       clean: true,
-      assetModuleFilename: isDev ? '[name][ext]' : '[name]_[fullhash:4][ext]',
+      assetModuleFilename: isDev ? "[name][ext]" : "[name]_[fullhash:4][ext]",
     },
-    devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
+    devtool: isDev ? "eval-cheap-module-source-map" : undefined,
     devServer: {
       port: 3000,
       compress: true,
@@ -40,75 +42,77 @@ module.exports = () => {
         {
           test: /\.(js|ts|jsx|tsx)?$/,
           exclude: /node_modules/,
-          include: path.resolve(__dirname, 'src'),
-          use: ['babel-loader'],
+          include: path.resolve(__dirname, "src"),
+          use: ["babel-loader"],
         },
         {
           test: /\.(css)$/,
           use: [
-            'style-loader',
+            "style-loader",
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
                 importLoaders: 2,
                 modules: {
-                  localIdentName: '[local]_[hash:base64:4]',
+                  localIdentName: "[local]_[hash:base64:4]",
                 },
               },
             },
-            'postcss-loader',
+            "postcss-loader",
           ],
           include: /\.module\.css$/,
         },
         {
           test: /\.(css)$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
           exclude: /\.module\.css$/,
         },
         {
           test: /\.svg$/i,
-          type: 'asset',
+          type: "asset",
           resourceQuery: /url/,
         },
         {
           test: /\.svg$/i,
           issuer: /\.[jt]sx?$/,
           resourceQuery: { not: [/url/] },
-          use: ['@svgr/webpack'],
+          use: ["@svgr/webpack"],
         },
         {
           test: /\.(ico|gif|png|jpg|jpeg)$/,
-          type: 'asset/resource',
+          type: "asset/resource",
           generator: {
-            filename: 'images/[name][ext]',
+            filename: "images/[name][ext]",
           },
         },
         {
           test: /\.(woff|woff2)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
       ],
     },
     resolve: {
       fallback: {
-        'react/jsx-runtime': 'react/jsx-runtime.js',
-        'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+        "react/jsx-runtime": "react/jsx-runtime.js",
+        "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
       },
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
     },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: isDev ? './styles/[name].css' : './styles/[name].[fullhash:4].css',
+        filename: isDev
+          ? "./styles/[name].css"
+          : "./styles/[name].[fullhash:4].css",
       }),
       new HtmlWebpackPlugin({
-        template: './public/index.html',
+        template: "./public/index.html",
       }),
       new webpack.DefinePlugin(envKeys),
       new CopyPlugin({
         patterns: [
           {
-            from: 'public/',
-            filter: (resourcePath) => resourcePath.indexOf('index.html') < 0,
+            from: "public/",
+            filter: (resourcePath) => resourcePath.indexOf("index.html") < 0,
           },
         ],
       }),
